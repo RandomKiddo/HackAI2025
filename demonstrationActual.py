91150d579238dc5b64cf56144f46c33af917223a
Nishanth Kunchala <nishanth.kunchala@gmail.com>
2025-02-22 17:59:43 -0500

    Added in initial camera positioning

--- demonstrationActual.py
+++ demonstrationActual.py
@@ -61,6 +61,7 @@ rotationMatrix = get_rotation_matrix([0, 0, 1], 90)
 dragonMesh.apply_transform(rotationMatrix)
 dragonMesh.apply_translation(dragonCoordinates)
 
+'''
 # Create a plotter using PyVista
 plotter = pv.Plotter()
 iss_pv_mesh = pv.wrap(issMesh)
@@ -82,3 +83,48 @@ plotter.add_mesh(line, color="lightgreen", opacity=1, line_width=3)
 plotter.show_axes()
 plotter.add_background_image('background.jpg')
 plotter.show()
+'''
+
+# Create a plotter using PyVista
+plotter = pv.Plotter()
+iss_pv_mesh = pv.wrap(issMesh)
+dragon_pv_mesh = pv.wrap(dragonMesh)
+
+# Overlay a 3D arrow, based off of our true value
+line = pv.Line(pointa = [0,-8,-8], pointb = [-1*prediction[2], -1*prediction[1], -1*prediction[0]])
+
+# Add STL geometry
+plotter.add_mesh(iss_pv_mesh, color="lightblue", opacity=1)
+plotter.add_mesh(dragon_pv_mesh, color="white", opacity=0.9)
+plotter.add_mesh(line, color="lightgreen", opacity=1, line_width=3)
+plotter.add_mesh(iss_pv_mesh, color="lightblue", opacity=1)
+plotter.add_mesh(dragon_pv_mesh, color="white", opacity=0.9)
+plotter.add_mesh(line, color="lightgreen", opacity=1, line_width=3)
+plotter.show_axes()
+plotter.add_background_image('background.jpg')
+
+# Set Initial Camera Position at the Tip of Dragon Shuttle
+initial_camera_position = [dragonCoordinates[0] - 3.15, dragonCoordinates[1], dragonCoordinates[2]]  # Slightly ahead of the nose
+focal_point = issCoordinates  # Look at the ISS
+view_up = [0, 1, 0]  # Keep the camera upright
+
+plotter.camera_position = [initial_camera_position, focal_point, view_up]
+plotter.show(auto_close=False)  # Keep window open for animation
+
+# Animate Pullback to Isometric View
+num_frames = 60  # Number of animation frames
+while True:
+    for i in range(num_frames):
+        # Interpolate between initial and final camera position
+        alpha = i
+        new_camera_position = [
+            initial_camera_position[0] + alpha * 10,  # Move back in X
+            initial_camera_position[1] - alpha * 10,  # Move back in Y
+            initial_camera_position[2] + alpha * 5,   # Move up in Z
+        ]
+        
+        plotter.camera_position = [new_camera_position, focal_point, view_up]
+        plotter.update()
+        plotter.render()  # Ensure updates appear smoothly
+
+plotter.close()  # Close the window after animation
