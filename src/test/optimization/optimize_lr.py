74efbabb91e02a79320d22dc726f40ac62cdae11
RandomKiddo <68043820+RandomKiddo@users.noreply.github.com>
2025-02-28 20:10:00 -0500

    HackAI Version LR Finder w/ GPU

--- src/test/optimization/optimize_lr.py
+++ src/test/optimization/optimize_lr.py
@@ -5,13 +5,17 @@ THIS FILE CURRENTLY RUNS TF.FUNCTION CODE WITH GRADIENT TAPE TO FIND THE
 OPTIMIZED LEARNING RATE FOR THE HACKAI MODEL OF THE MHN. IT OUTPUTS A 
 PNG PLOT SHOWING LOSS VS. LEARNING RATE TO EXAMINE FOR OPTIMAL LEARNING
 RATE.
+
+THIS FILE IS OPTIMIZED FOR USE WITH A GPU WITH MEMORY GROWTH, UTILIZING
+TENSORFLOW MIXED PRECISION POLICY, AND NOT USING THE CACHED DATASET TO
+SAVE RAM. 
 """
 
 
 import os
 import random
 import json
-import pickle
+import gc
 
 import tensorflow as tf
 import kagglehub as kh
@@ -20,6 +24,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from tqdm import tqdm
+
+policy = tf.keras.mixed_precision.Policy('mixed_float16')
+tf.keras.mixed_precision.set_global_policy(policy)
+
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), 'Physical GPUs,', len(logical_gpus), 'Logical GPUs')
+    except RuntimeError as e:
+        print(e)
 
 tf.random.set_seed(42)
 
@@ -30,7 +48,7 @@ df = pd.read_csv(os.path.join(path, 'train.csv'))
 n_imgs = len(df)
 
 img_width = img_height = 512
-batch_size = 32
+batch_size = 64
 
 image_paths = []
 for f in os.listdir(os.path.join(path, 'train')):
@@ -98,7 +116,7 @@ def create_dataset(paths, dists, locs):
     dataset = tf.data.Dataset.from_tensor_slices((paths, dists, locs))
     dataset = dataset.map(lambda i, d, xy: (as_image(i), {'distance': d, 'x': xy[0], 'y': xy[1]}), num_parallel_calls=tf.data.AUTOTUNE)
     dataset = dataset.batch(batch_size)
-    dataset = dataset.cache()
+    #dataset = dataset.cache()
     dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
     return dataset
 
@@ -155,6 +173,8 @@ def lr_schedule(epoch):
 @tf.function
 def train_step(model, optimizer, inputs, targets, loss_object):
     with tf.GradientTape() as tape:
+        tape.watch(inputs)
+
         l, m, r = model(inputs)
         loss = loss_object(targets['distance'], l)
         loss += loss_object(targets['x'], m)
@@ -176,6 +196,9 @@ for epoch in range(100):
 
         loss = train_step(model, optimizer, inputs, targets, loss_object)
         total_loss += loss
+
+        del inputs, targets, loss
+        gc.collect()
     
     lrs.append(lr)
     losses.append(total_loss.numpy())
@@ -185,5 +208,5 @@ plt.xscale('log')
 plt.xlabel('Learning Rate')
 plt.ylabel('Loss')
 plt.title('Learning Rate Finder')
-plt.savefig('lrfinder.py')
+plt.savefig('lrfinder.png')
 plt.show()
